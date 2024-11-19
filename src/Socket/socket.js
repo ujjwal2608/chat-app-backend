18a845397aff1d7efb1c6f194987f5646160b81c
ujjwal.aggarwal <ujjwal.aggarwal@geekyants.com>
2024-11-19 18:32:41 +0530

    event call

--- src/Socket/socket.js
+++ src/Socket/socket.js
@@ -28,6 +28,18 @@ io.on("connection", (socket) => {
 	io.emit("getOnlineUsers", Object.keys(userSocketMap));
 	console.log("online users: ",Object.keys(userSocketMap));
 	// socket.on() is used to listen to the events. can be used both on client and server side
+	socket.on("call", (data) => {
+		const { receiverId } = data; // Assuming data contains receiverId
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		
+		if (receiverSocketId) {
+			// Emit 'pickupCall' event to the specific user
+			io.to(receiverSocketId).emit("pickupCall", { from: userId });
+			console.log(`Call event sent from ${userId} to ${receiverId}`);
+		} else {
+			console.log(`User ${receiverId} is not connected`);
+		}
+	});
 	socket.on("disconnect", () => {
 		delete userSocketMap[userId];
 		console.log(`User ${userId} disconnected`);
